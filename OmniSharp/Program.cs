3514b4d209c0404621f43f6e50985511deaaa015
Jeremy Burns <jeremyaburns@gmail.com>
2014-05-08 19:59:44 -0600

    Removed keypress required on existing instance.

--- OmniSharp/Program.cs
+++ OmniSharp/Program.cs
@@ -111,7 +111,6 @@ namespace OmniSharp
 				if(e is SocketException || e is HttpListenerException)
 				{
 					logger.Error("Detected an OmniSharp instance already running on port " + port + ". Press a key.");
-					Console.ReadKey();
 					return;
 				}
 				throw;
